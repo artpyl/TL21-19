501949a80da12183cf55697ddb04c3db7f0bd829
Markosgi <94316555+Markosgi@users.noreply.github.com>
2021-12-17 18:17:43 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,15 +4,30 @@ const bodyparser = require('body-parser')
 const app = express()
 const baseURL = '/interoperability/api';
 app.use(bodyparser.json()) //for app.post & fetch body as json
-app.listen(port, '192.168.2.7', () => {
+app.listen(port,() => {
   console.log("hi");
 });
 
 app.get(baseURL,(req,res) =>{
   res.send("hello")
 });
-const PassesPerStation = require("./endpoints/PassesPerStation.js")
+
+//load all endpoints
+const PassesPerStation = require("./endpoints/PassesPerStation.js");
+const PassesAnalysis = require("./endpoints/PassesAnalysis.js");
+const PassesCost = require("./endpoints/PassesCost.js");
+const ChargesBy = require("./endpoints/ChargesBy.js");
+//const Healthcheck = require("./endpoints/healthcheck.js");
+const ResetPasses = require("./endpoints/resetpasses.js");
+const ResetStations = require("./endpoints/resetstations.js");
+const ResetVehicles = require("./endpoints/resetvehicles.js");
+
+//bind all endpoints to app router
 app.use(baseURL, PassesPerStation);
-//app.use(baseURL, PassesAnalysis);
-//app.use(baseURL, PassesCost);
-//app.use(baseURL, ChargesBy);
+app.use(baseURL, PassesAnalysis);
+app.use(baseURL, PassesCost);
+app.use(baseURL, ChargesBy);
+//app.use(baseURL, Healthchceck);
+app.use(baseURL, ResetPasses);
+app.use(baseURL, ResetStations);
+app.use(baseURL, ResetVehicles);
