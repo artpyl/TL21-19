77544389fa444e69290b4a9ee400c4201d2fe794
artpyl <83124209+artpyl@users.noreply.github.com>
2021-12-20 00:18:07 +0200

    Add files via upload

---
+++ backend/endpoints/Admin/ResetPasses.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+var mysql = require('mysql');
+
+function resetpasses (req,res){
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "art",
+        password: "password",
+        database: "freedomepass",
+        multipleStatements: true
+    });
+
+    con.connect(function(err){
+        if (err) throw err;
+        console.log("connected");
+        let myquery = "DELETE FROM passes"
+        con.query(myquery, function (err, result, fields){
+            if (err) {
+              return res.send({"status" : "failed"});
+            }
+            res.send({"status" : "OK"});
+        });
+        con.end(function(err) {
+            if (err) {
+            return console.log('error:' + err.message);
+            }
+            console.log('Close the database connection.');
+        });
+    });
+}
+
+router.post('/ResetPasses', resetpasses);
+module.exports = router;
