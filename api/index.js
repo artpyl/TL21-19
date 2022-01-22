a5a2226b159deb3973d949829692fabd38dcad31
SnifflePiffle <el18026@mail.ntua.gr>
2022-01-22 12:51:40 +0200

    Adding HTTPS support to API

--- api/index.js
+++ api/index.js
@@ -1,17 +1,35 @@
 const express = require("express");
 const port = 9103;
 const bodyparser = require('body-parser')
+const cors = require('cors');
 const app = express()
 const baseURL = '/interoperability/api';
 app.use(bodyparser.json())
-app.listen(port, () => {
-  console.log("hi");
-});
+app.use(cors());
+
+const fs = require('fs');
+const key = fs.readFileSync('./localhost.decrypted.key');
+const cert = fs.readFileSync('./localhost.crt');
 
 var path = require("path");
 app.get(baseURL,(req,res) =>{
-  res.sendFile(path.join(__dirname, '..', '/frontend/', 'index.html'));
+  res.sendFile(baseURL);
 });
+
+const https = require('https');
+const server = https.createServer({ key, cert }, app);
+
+server.listen(port, () => {
+  console.log(`Server is listening on https://localhost:${port}`);
+});
+
+
+/* app.listen(port, () => {
+  console.log("hi");
+});
+*/
+
+
 
 const PassesPerStation = require("../backend/endpoints/PassesPerStation.js");
 const PassesAnalysis = require("../backend/endpoints/PassesAnalysis.js");
@@ -21,6 +39,8 @@ const Healthcheck = require("../backend/endpoints/Admin/healthcheck.js");
 const ResetPasses = require("../backend/endpoints/Admin/ResetPasses.js");
 const ResetStations = require("../backend/endpoints/Admin/ResetStations.js");
 const ResetVehicles = require("../backend/endpoints/Admin/ResetVehicles.js");
+const PassesAnalysis2 = require("../backend/endpoints/PassesAnalysis2.js");
+
 
 app.use(baseURL, PassesPerStation);
 app.use(baseURL, PassesAnalysis);
@@ -30,3 +50,12 @@ app.use(baseURL, Healthcheck);
 app.use(baseURL, ResetPasses);
 app.use(baseURL, ResetStations);
 app.use(baseURL, ResetVehicles);
+app.use(baseURL, PassesAnalysis2);
+
+app.set('views', path.join(__dirname, 'views'));
+//app.engine('ejs', ejs.renderFile);
+app.set('view engine', 'ejs');
+var router = express.Router();
+router.get('/PassesAnalysis2/:op1_ID/:op2_ID/:date_from/:date_to', function (req,res) {
+  res.render('/PassesAnalysis.ejs');
+})
