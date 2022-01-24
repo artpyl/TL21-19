b7aa06a124959742c0d6b4fa605751f852a67b7b
SnifflePiffle <94315580+SnifflePiffle@users.noreply.github.com>
2022-01-24 15:28:03 +0200

    Add files via upload

--- backend/endpoints/ChargesBy.js
+++ backend/endpoints/ChargesBy.js
@@ -4,24 +4,41 @@ var mysql = require('mysql');
 
 function getData4 (req,res){
     var con = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "root",
-        database: "freedomepass",
-        multipleStatements: true
-    });
+      host: "localhost",
+      user: "art",
+      password: "password",
+      database: "freedomepass",
+      multipleStatements: true
+  });
 
     con.connect(function(err){
             if (err) throw err;
             console.log("connected");
             let date_from = req.params.date_from.slice(0,4) +'-' + req.params.date_from.slice(4,6) + '-' + req.params.date_from.slice(6,8) + ' 00:00:00';
             let date_to = req.params.date_to.slice(0,4) +'-' + req.params.date_to.slice(4,6) + '-' + req.params.date_to.slice(6,8) + ' 00:00:00';
-            let myquery4 = "SELECT tagProvider as VisitingOperator, COUNT(*) as NumberOfPasses, CAST(FORMAT(SUM(charge),2) as FLOAT) as PassesCost FROM passes, vehicles, stations WHERE stations.stationProvider= " + "'" + req.params.op_ID + "'"
+            let myquery4 = "SELECT stationProvider as VisitingOperator, CAST(FORMAT(SUM(charge),2) as FLOAT) as PassesCost FROM passes, vehicles, stations WHERE stations.stationProvider<> " + "'" + req.params.op_ID + "'"
+                         + "AND vehicles.tagProvider = " + "'" + req.params.op_ID + "'"
+                         + "AND passes.stationRef = stations.stationID AND passes.vehicleRef = vehicles.vehicleID AND timestamp BETWEEN " + "'" + date_from + "'" + "AND" + "'" + date_to + "'" + "GROUP BY stationProvider;"
+
+                         + "SELECT tagProvider as VisitingOperator, COUNT(*) as NumberOfPasses, CAST(FORMAT(SUM(charge),2) as FLOAT) as PassesCost FROM passes, vehicles, stations WHERE stations.stationProvider= " + "'" + req.params.op_ID + "'"
                          + "AND vehicles.tagProvider<> " + "'" + req.params.op_ID + "'"
                          + "AND passes.stationRef = stations.stationID AND passes.vehicleRef = vehicles.vehicleID AND timestamp BETWEEN " + "'" + date_from + "'" + "AND" + "'" + date_to + "'" + "GROUP BY tagProvider; SELECT REPLACE(CURRENT_TIMESTAMP,'T','') as RequestTimestamp;";
             con.query(myquery4, function (err, result, fields){
+                  for (let i = 0; i < result[1].length; i++) {
+                    for(let j = 0; j < result[0].length; j++) {
+                    if (result[1][i]["VisitingOperator"] == result[0][j]["VisitingOperator"]) {
+                      if(result[1][i]["PassesCost"] - result[0][j]["PassesCost"] > 0) {
+                        result[1][i]["PassesCost"] = result[1][i]["PassesCost"] - result[0][j]["PassesCost"];
+                        result[1][i]["PassesCost"] =  result[1][i]["PassesCost"].toFixed(2);
+                      }
+                      else result[1][i]["PassesCost"] = 0;
+                      break;
+                    }
+                  }
+
+                }
                 if (err) throw err;
-                let json = {op_ID : req.params.op_ID, RequestTimestamp: result[1][0]["RequestTimestamp"], PeriodFrom : date_from , PeriodTo :  date_to , PPOList: result[0]};
+                let json = {op_ID : req.params.op_ID, RequestTimestamp: result[2][0]["RequestTimestamp"], PeriodFrom : date_from , PeriodTo :  date_to , PPOList: result[1]};
                 if (req.query.format == 'json')
                   res.send(json);
                 else {
